d7e5cb7e9d1c84f7dde330e96a5171ff4b1f73b7
alinabodnar1 <malina.bodnar@gmail.com>
2023-10-20 19:24:16 +0300

    fixed index.html

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+
+// there are files: styles.css, logo.svg, browser-app.js in this folder
+app.use(express.static('./public'))
+
+// app.get('/', (req,res) =>{
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html')); // here resolve = join
+// })
+
+app.all('*',(req,res) =>{
+  res.status(404).send('Resource not found')
+});
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000...");
--- final/2-express-basics.js
+++ final/2-express-basics.js
@@ -22,3 +22,7 @@ app.get('/about', (req, res) =>{
 app.all('*', (req, res) =>{
   res.status(404).send('<h1>Resource not found </h1>')
 })
+
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000...");
+});
